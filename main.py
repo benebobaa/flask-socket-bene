729b910d36a89ee172b4017a9ce6114355afcd6c
unknown <benediktus@students.amikom.ac.id>
2023-06-02 22:35:03 +0700

    test streaming tweet

--- main.py
+++ main.py
@@ -1,15 +1,101 @@
-from flask import Flask, jsonify
-import os
+import requests, json
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+
+# To set your enviornment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAGohhgEAAAAAoxJecHy%2FIZJkU3oKQnHrfUz1AoY%3D9y80NOdXNdZ9DggBxe4Cknx9Ox8qwl2e164deKRQZRU0dRfLGg"
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2FilteredStreamPython"
+    return r
+
+
+def get_rules():
+    response = requests.get(
+        "https://api.twitter.com/2/tweets/search/stream/rules", auth=bearer_oauth
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot get rules (HTTP {}): {}".format(response.status_code, response.text)
+        )
+    print(json.dumps(response.json()))
+    return response.json()
+
+
+def delete_all_rules(rules):
+    if rules is None or "data" not in rules:
+        return None
+
+    ids = list(map(lambda rule: rule["id"], rules["data"]))
+    payload = {"delete": {"ids": ids}}
+    response = requests.post(
+        "https://api.twitter.com/2/tweets/search/stream/rules",
+        auth=bearer_oauth,
+        json=payload
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot delete rules (HTTP {}): {}".format(
+                response.status_code, response.text
+            )
+        )
+    print(json.dumps(response.json()))
+
+
+def set_rules(delete):
+    # You can adjust the rules if needed
+    sample_rules = [
+        {"value": "kebakaran hutan"},
+       
+    ]
+    payload = {"add": sample_rules}
+    response = requests.post(
+        "https://api.twitter.com/2/tweets/search/stream/rules",
+        auth=bearer_oauth,
+        json=payload,
+    )
+    if response.status_code != 201:
+        raise Exception(
+            "Cannot add rules (HTTP {}): {}".format(response.status_code, response.text)
+        )
+    print(json.dumps(response.json()))
+
+
+def get_stream():
+    response = requests.get(
+        "https://api.twitter.com/2/tweets/search/stream?tweet.fields=created_at&user.fields=profile_image_url&expansions=author_id", auth=bearer_oauth, stream=True,
+    )
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot get stream (HTTP {}): {}".format(
+                response.status_code, response.text
+            )
+        )
+    for response_line in response.iter_lines():
+        if response_line:
+            json_response = json.loads(response_line)
+            print(json.dumps(json_response, indent=4, sort_keys=True))
+            emit('server_response', str(json.dumps(json_response['data']['text'])), broadcast=True)
+            socketio.sleep(8)
 
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template('index.html')
+
 
 @socketio.on('connect')
 def handle_connect():
@@ -19,12 +105,20 @@ def handle_connect():
 def handle_disconnect():
     print('Client disconnected')
 
-
 @socketio.on('client_event')
 def handle_client_event(data):
     print('Received event from client:', data)
+    # Do some processing here
+    # Emit a response event back to the client
+    rules = get_rules()
+    print('cek 1')
+    delete = delete_all_rules(rules)
+    print('cek 2')
+    set = set_rules(delete)
     print('cek 3')
     emit('server_response', 'Hello from the server', broadcast=True)
+    get_stream()
+
 
 
 if __name__ == '__main__':
